2cb14cf0bc7ea833e4fab8406f8a72561da8046f
winston kiprop <winstonekip50@gmail.com>
2022-08-26 16:16:26 +0300

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex MALE,
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears VALUES(1," Mr. Chocolate",3,"M","white","cool","true");
+INSERT INTO bears VALUES(2,"Rowdy",7,"M","white","cool","true");
+INSERT INTO bears VALUES(3," Tabitha",6,"F","black","cheerful","false");
+INSERT INTO bears VALUES(4," Sargeant Brown",3,"M","black","dull","true");
+INSERT INTO bears VALUES(5,"Melissa",13,"F","white","cool","false");
+INSERT INTO bears VALUES(6,"Grinch",2,"M","black","cheerful","true");
+INSERT INTO bears VALUES(7,"Wendy",6,"F","white","cool","false");
+INSERT INTO bears VALUES(8,NULL,2,"M","white","sad","false");
